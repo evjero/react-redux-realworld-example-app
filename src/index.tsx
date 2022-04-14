90ee1d0a626d9be7ff5a1c4902c347d6cc02bb44
evjero <evjero.git@gmail.com>
2022-04-13 20:22:44 -0400

    Init tsconfig; update index

--- src/index.tsx
+++ src/index.tsx
@@ -8,20 +8,23 @@ import App from './components/App';
 import store from './app/store';
 
 // const _history = createBrowserHistory()
-
+//eslint-disable-next-line
+//@ts-ignore
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        {/* <ConnectedRouter history={history}> */}
         <App />
-        {/* </ConnectedRouter> */}
       </Router>
     </Provider>
   </React.StrictMode>
 );
 // https://www.cypress.io/blog/2018/11/14/testing-redux-store/
 /* istanbul ignore else */
+//eslint-disable-next-line
+//@ts-ignore
 if (window.Cypress) {
+  //eslint-disable-next-line
+  //@ts-ignore
   window.store = store;
 }
